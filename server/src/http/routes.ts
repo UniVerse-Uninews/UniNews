78a17590517d6800caac82551245e42cb0549057
Eurosphyr <miguel.mmgg2016@gmail.com>
2024-05-06 17:43:57 -0300

    Created the route /users

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
+import { register } from "./controllers/register";
 
 export async function appRoutes(app: FastifyInstance) {
   app.get("/", async () => {
     return { hello: "world" };
     });
-}
+    app.post("/users", register);
+  }
