de110b29b8cef25e6305ae8c5bc623023bcaf0f3
Eurosphyr <miguel.mmgg2016@gmail.com>
2024-05-12 07:56:34 -0300

    Created get user profile

---
+++ server/src/services/get-user-profile.ts
@@ -0,0 +1,30 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository ";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { User } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface GetUserProfileUseCaseRequest {
+  userId: string;
+}
+
+interface GetUserProfileUseCaseResponse {
+  user: User;
+}
+
+export class GetUserProfileUseCase {
+  constructor(private usersRepository: PrismaUsersRepository) {}
+
+  async execute({
+    userId,
+  }: GetUserProfileUseCaseRequest): Promise<GetUserProfileUseCaseResponse> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new ResourceNotFoundError();
+    }
+    return {
+      user,
+    };
+  }
+}
