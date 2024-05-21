34fc547a17eca062f65bd8475f5ae6f8140028e4
Minieu4 <gustavo.polido@unesp.br>
2024-05-21 09:31:30 -0300

    Create controller get-all-university

---
+++ server/src/http/controllers/get-all-university.ts
@@ -0,0 +1,14 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { PrismaUniversityRepository } from "../../repositories/prisma/prisma-university-repository";
+import { University } from "@prisma/client";
+
+export async function getAllUniversityController(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const universityRepository = new PrismaUniversityRepository(); 
+    const allUniversities = await universityRepository.findAll();
+    reply.status(200).send(allUniversities);
+  } catch (error) {
+    console.error("Error retrieving all universities:", error);
+    reply.status(500).send({ error: "Internal Server Error" });
+  }
+}
