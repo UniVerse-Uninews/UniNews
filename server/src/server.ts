3b71f95e8fa8a58fe245ab7b62c1f735450b9354
Eurosphyr <miguel.mmgg2016@gmail.com>
2024-05-26 16:42:26 -0300

    Change the .env config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,9 @@
 import { app } from "./app";
-import { env } from "./env";
 
 
 app
   .listen({
     host: "0.0.0.0",
-    port: env.PORT,
+    port: process.env.PORT,
   })
   .then(() => console.log("🚀HTTP Server is running!"));
