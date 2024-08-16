21fb8405b4c6024126e402063b769923d5d21bda
RyandroMoriizumi <146085407+RyandroMoriizumi@users.noreply.github.com>
2024-08-16 12:09:12 -0300

    Update header.tsx

--- client/src/components/addHeader/header.tsx
+++ client/src/components/addHeader/header.tsx
@@ -1,12 +1,17 @@
 import { styles } from './styleheader';
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar,  SafeAreaView, Text } from 'react-native';
 import { Container, NameBlue, Name, Line } from '../../theme/style';
 import ImageViewer from '../addImageViewer/ImageViewer';
 
 export function Header() {
   const dirImagem = require('../../../assets/imagens/tcc-logo-quadrado-sem-fundo.png');
   return (
+    <>
+    <StatusBar></StatusBar>
+     <SafeAreaView style={styles.container2}>
+      
+     </SafeAreaView>
     <Container style={styles.container}>
       <View style={styles.cabecalho}>
         <View>
@@ -17,5 +22,6 @@ export function Header() {
       </View>
       <Line style={styles.line} />
     </Container>
+    </>
   );
 }
