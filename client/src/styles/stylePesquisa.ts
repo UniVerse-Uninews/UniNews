af2ec49b3774daabb9862b84d7310e9581742f2e
Minieu4 <143809473+Minieu4@users.noreply.github.com>
2024-09-04 11:58:05 -0300

    chore: Update drawer component styles and responsive dimensions

--- client/src/styles/stylePesquisa.ts
+++ client/src/styles/stylePesquisa.ts
@@ -2,12 +2,15 @@ import { StyleSheet } from 'react-native';
 import { responsiveScreenHeight } from 'react-native-responsive-dimensions';
 
 export const styles = StyleSheet.create({
-    container1:{},
+    container1:{
+        flex: 1,
+    },
     container2:{
         flexDirection:'row',
     },
     container3:{
-        flexDirection:'column'
+        flexDirection:'column',
+        position: 'relative',
     },
     description:{
         borderWidth:3
@@ -23,4 +26,7 @@ export const styles = StyleSheet.create({
         width: 15
     },
     dropdown:{},
+    text:{
+        position: 'relative',
+    },
 });
