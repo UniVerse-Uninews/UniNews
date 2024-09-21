3f80fc6d1cbd23379f311269468cb931c355f124
RyandroMoriizumi <146085407+RyandroMoriizumi@users.noreply.github.com>
2024-09-21 09:22:09 -0300

    Create styleSobre.ts

---
+++ client/src/styles/styleSobre.ts
@@ -0,0 +1,42 @@
+
+import { StyleSheet } from 'react-native';
+import { responsiveFontSize } from 'react-native-responsive-dimensions';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    
+      
+    },
+    sobre:{
+        color:'#ffff',
+        alignContent:'center',
+        justifyContent:'center',
+        textAlign:'center',
+        paddingVertical: '3%',
+        fontSize:24
+    },
+    perfil:{
+        flexDirection:'row',
+        textAlign:'center',
+        alignItems:'center',
+        marginBottom:'7%'
+
+
+        
+    },
+    foto:{
+        borderWidth:1,
+        height:80,
+        width:80,
+        borderRadius:50,
+        marginRight:'5%'
+    },
+   
+
+   
+     
+     
+    
+  });
+  
