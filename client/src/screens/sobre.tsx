77c61c0e1a1115a66827f187442974d26d5b1f1e
RyandroMoriizumi <146085407+RyandroMoriizumi@users.noreply.github.com>
2024-09-23 10:34:35 -0300

    Update sobre.tsx

--- client/src/screens/sobre.tsx
+++ client/src/screens/sobre.tsx
@@ -11,6 +11,7 @@ import {
   Subbox,
 } from '../theme/style';
 import { styles } from '../styles/styleSobre';
+const dirImagem = require('../../assets/imagens/tcc-logo-quadrado-sem-fundo.png');
 
 
 export  function Sobre() {
@@ -20,7 +21,22 @@ export  function Sobre() {
         <Header />
         <Container style={styles.container}>
         <ScrollView>
-            <Name style={styles.sobre}>Quem somos</Name>
+            <Name style={styles.sobre}>Sobre o Projeto</Name>
+            <View style={styles.titulo}>
+              <Image style={styles.Logo} source={dirImagem} />
+              
+              <Name style={styles.subtitulo1} >Este projeto trata-se de um veiculador mobile de notícias e pesquisas universitárias. </Name>
+            </View>
+
+            <Name style={styles.subtitulo}>O principal intuito desta plataforma é não apenas facilitar o acesso a informações acadêmicas, mas também apresentá-las de maneira confiável e diretamente da fonte, direcionadas aos interesses do usuário.</Name>
+          
+            <Name style={styles.sobre}>Quem Somos</Name>
+            <View style={styles.titulo}>
+              <Image style={styles.Logo} source={dirImagem} />
+              
+              <Name style={styles.subtitulo1} >Ryandro maravilhoso. </Name>
+            </View>
+            
 
             
             <View style={styles.perfil}>
