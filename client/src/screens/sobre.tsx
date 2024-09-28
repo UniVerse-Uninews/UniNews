6728b9b521390e8ccc7aa9ba93beaa009c02e754
RyandroMoriizumi <146085407+RyandroMoriizumi@users.noreply.github.com>
2024-09-28 09:23:49 -0300

    Update sobre.tsx

--- client/src/screens/sobre.tsx
+++ client/src/screens/sobre.tsx
@@ -7,6 +7,7 @@ import {
   Name,
   Container,
   NameBlue,
+  NameBlueDark,
   Textbox,
   Subbox,
 } from '../theme/style';
@@ -21,7 +22,8 @@ export  function Sobre() {
         <Header />
         <Container style={styles.container}>
         <ScrollView>
-            <Name style={styles.sobre}>Sobre o Projeto</Name>
+            <NameBlueDark style={styles.sobre}>Sobre Nós</NameBlueDark>
+            <NameBlue style={styles.titulo1}>UNINEWS</NameBlue>
             <View style={styles.titulo}>
               <Image style={styles.Logo} source={dirImagem} />
               
@@ -30,39 +32,98 @@ export  function Sobre() {
 
             <Name style={styles.subtitulo}>O principal intuito desta plataforma é não apenas facilitar o acesso a informações acadêmicas, mas também apresentá-las de maneira confiável e diretamente da fonte, direcionadas aos interesses do usuário.</Name>
           
-            <Name style={styles.sobre}>Quem Somos</Name>
+            <NameBlueDark style={styles.sobre}>Quem Somos</NameBlueDark>
+            <NameBlue style={styles.titulo1}>UNIVERSE</NameBlue>
             <View style={styles.titulo}>
               <Image style={styles.Logo} source={dirImagem} />
               
               <Name style={styles.subtitulo1} >Ryandro maravilhoso. </Name>
             </View>
             
-
+            <NameBlueDark style={styles.sobre}>Desenvolvedores</NameBlueDark>
             
             <View style={styles.perfil}>
-                <View style={styles.foto}></View>
-                <View >
-                <Name>Ryandro Lindo</Name> 
-                <Name>Lindo Demais</Name>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
                 </View>
-               
-            </View>
-
-            <View style={styles.perfil}>
-                <View style={styles.foto}></View>
-                <View >
-                <Name>Ryandro Lindo</Name> 
-                <Name>Gostoso demais</Name>
+                <View  style={styles.texto}>
+                <NameBlue>Ana Lara Picalio</NameBlue> 
+                <NameBlue>Desenvolvedora</NameBlue>
                 </View>
             </View>
 
             <View style={styles.perfil}>
-                <View style={styles.foto}></View>
-                <View >
-                <Name>Ryandro Lindo</Name> 
-                <Name>Muito Gato</Name>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
                 </View>
-                
+                <View  style={styles.texto}>
+                <NameBlue>Arthur Ximenes Orsolini</NameBlue> 
+                <NameBlue>PM/PO</NameBlue>
+                </View>
+            </View>
+
+            <View style={styles.perfil}>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
+                </View>
+                <View  style={styles.texto}>
+                <NameBlue>Carol Xavier Mazon</NameBlue> 
+                <NameBlue>Desenvolvedora</NameBlue>
+                </View>
+            </View>
+
+            <View style={styles.perfil}>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
+                </View>
+                <View  style={styles.texto}>
+                <NameBlue>Gustavo Carvalho Polido</NameBlue> 
+                <NameBlue>Desenvolvedor</NameBlue>
+                </View>
+            </View>
+
+            <View style={styles.perfil}>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
+                </View>
+                <View  style={styles.texto}>
+                <NameBlue>Miguel Angelo De Lima Godoi</NameBlue> 
+                <NameBlue>Lider Técnico</NameBlue>
+                </View>
+            </View>
+
+            <View style={styles.perfil}>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
+                </View>
+                <View  style={styles.texto}>
+                <NameBlue>Pedro A. O. G. Ribeiro</NameBlue> 
+                <NameBlue>Desenvolvedora</NameBlue>
+                </View>
+            </View>
+
+            <View style={styles.perfil}>
+                <View style={styles.foto1}>
+                  <View style={styles.foto}>
+                    
+                  </View>
+                </View>
+                <View  style={styles.texto}>
+                <NameBlue>Ryandro Zerlin Moriizumi</NameBlue> 
+                <NameBlue>Lider UX/UI</NameBlue>
+                </View>
             </View>
          
         </ScrollView>
